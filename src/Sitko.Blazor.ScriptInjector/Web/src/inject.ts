4a1ea1557ef8c108d645667c6ebe6530ca48fdca
George Drak <sonicgd@gmail.com>
2023-12-22 20:56:27 +0500

    fix: correctly load multiple instances

--- src/Sitko.Blazor.ScriptInjector/Web/src/inject.ts
+++ src/Sitko.Blazor.ScriptInjector/Web/src/inject.ts
@@ -11,61 +11,80 @@ interface Window {
 if (!window.scriptInjectFunction) {
   function injectElement(instance: any, id: string, element: HTMLElement): void {
     element.onload = () => {
-      console.debug(id, 'loaded');
-      instance.invokeMethodAsync("RequestLoadedAsync", id);
+      onLoad(id, instance);
     }
     // if it fails, return reject
     element.onerror = () => {
-      console.debug(id, 'failed');
-      instance.invokeMethodAsync("RequestFailedAsync", id);
+      onError(id, instance);
     }
     // scripts will load at end of body
     document["body"].appendChild(element);
   }
 
-  function checkElement(id: string) {
-    if (document.getElementById(id)) {
-      throw new Error(`Resource ${id} already loaded`);
-    }
+  function isAlreadyLoaded(id: string) {
+    return !!document.getElementById(id);
+
+  }
+
+  function onLoad(id: string, instance: any): void {
+    console.debug(id, 'loaded');
+    instance.invokeMethodAsync("RequestLoadedAsync", id);
+  }
+
+  function onError(id: string, instance: any): void {
+    console.debug(id, 'failed');
+    instance.invokeMethodAsync("RequestFailedAsync", id);
   }
 
   window.scriptInjectFunction = function (instance, id, scriptPath) {
     console.debug("Inject script from url");
     const elementId = 'js-' + id;
-    checkElement(elementId);
-    const script = document.createElement("script");
-    script.src = scriptPath;
-    script.type = "text/javascript";
-    script.id = elementId;
-    injectElement(instance, id, script);
+    if (!isAlreadyLoaded(elementId)) {
+      const script = document.createElement("script");
+      script.src = scriptPath;
+      script.type = "text/javascript";
+      script.id = elementId;
+      injectElement(instance, id, script);
+    } else {
+      onLoad(id, instance);
+    }
   }
   window.scriptInlineInjectFunction = function (instance, id, scriptContent) {
     console.debug("Inject inline script");
     const elementId = 'js-' + id;
-    checkElement(elementId);
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.id = elementId;
-    script.text = scriptContent;
-    injectElement(instance, id, script);
+    if (!isAlreadyLoaded(elementId)) {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.id = elementId;
+      script.text = scriptContent;
+      injectElement(instance, id, script);
+    } else {
+      onLoad(id, instance);
+    }
   }
   window.cssInjectFunction = function (instance, id, cssPath) {
     console.debug("Inject css from url");
     const elementId = 'css-' + id;
-    checkElement(elementId);
-    const linkElement = document.createElement("link");
-    linkElement.href = cssPath;
-    linkElement.rel = "stylesheet";
-    linkElement.id = elementId;
-    injectElement(instance, id, linkElement);
+    if (!isAlreadyLoaded(elementId)) {
+      const linkElement = document.createElement("link");
+      linkElement.href = cssPath;
+      linkElement.rel = "stylesheet";
+      linkElement.id = elementId;
+      injectElement(instance, id, linkElement);
+    } else {
+      onLoad(id, instance);
+    }
   }
   window.cssInlineInjectFunction = function (instance, id, css: string) {
     console.debug("Inject inline css");
     const elementId = 'css-' + id;
-    checkElement(elementId);
-    const styleElement = document.createElement('style');
-    styleElement.id = elementId;
-    styleElement.textContent = css;
-    injectElement(instance, id, styleElement);
+    if (!isAlreadyLoaded(elementId)) {
+      const styleElement = document.createElement('style');
+      styleElement.id = elementId;
+      styleElement.textContent = css;
+      injectElement(instance, id, styleElement);
+    } else {
+      onLoad(id, instance);
+    }
   }
 }
